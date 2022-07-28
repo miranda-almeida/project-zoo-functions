4c622760123ee5a320fae997c088151931b217be
miranda <magui@duck.com>
2022-07-28 16:38:37 -0300

    requisito 6 e 7: testes para a função handlerElephants

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,28 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verifica se a função retorna undefined ao receber um parâmetro vazio', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  it('Verifica se o parâmetro recebido é string', () => {
+    expect(handlerElephants(24)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Verifica se o parâmetro count retorna 4', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Verifica se o parâmetro names retorna o array com o nome de todos os elefantes', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('Verifica se o parâmetro averageAge retorna 10.5 como média da idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+  it('Verifica se o parâmetro location retorna NW', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('Verifica se o parâmetro popularity retorna 5', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+  it('Verifica se o parâmetro availability retorna um array com os dias de visitação disponíveis', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+});
