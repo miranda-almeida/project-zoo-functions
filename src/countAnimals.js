22d1c9848b5beba3ef60b338ce767cacf32a6d89
miranda <magui@duck.com>
2022-07-28 19:42:22 -0300

    requisito 5: função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
+const counter = () =>
+  data.species.reduce((accumulator, specie) => {
+    accumulator[specie.name] = specie.residents.length;
+    return accumulator;
+  }, {});
+
+const finder = (names) => data.species.find((specie) => specie.name === names.specie).residents;
+
+const gender = (names) => finder(names).filter((residents) => residents.sex === names.sex);
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return counter();
+  }
+  if (animal.sex) {
+    return gender(animal).length;
+  }
+  if (!animal.sex) {
+    return finder(animal).length;
+  }
 }
 
 module.exports = countAnimals;
