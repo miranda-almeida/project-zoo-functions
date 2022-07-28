ac3c668df033cbaeb19bb815a3b230ccf1ebe2f7
miranda <magui@duck.com>
2022-07-28 17:51:08 -0300

    requisito 13: cobertura de testes de 95% para getOpeningHours

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -7,10 +7,16 @@ describe('Testes da função getOpeningHours', () => {
   it('Verifica se o zoo retorna como aberto no domingo às 8:00AM', () => {
     expect(getOpeningHours('Sunday', '08:00-AM')).toBe('The zoo is open');
   });
-  // it('Verifica se o segundo paramentro como string retorna o Error', () => {
-  //   expect(getOpeningHours('Wednesday', 'Dez')).toThrow(Error);
-  // });
-  // it('Verifica se o parâmetro referente a hora está em um valor entre 0 e 12', () => {
-  //   expect(getOpeningHours('Wednesday', '13:05-PM')).toThrow(Error);
-  // });
+  it('Verifica se o segundo paramentro como string retorna o Error', () => {
+    expect(getOpeningHours('Wednesday', 'Dez')).toThrow(console.log(Error));
+  });
+  it('Verifica se o parâmetro referente a hora está em um valor entre 0 e 12', () => {
+    expect(getOpeningHours('Wednesday', '13:05-PM')).toThrow(console.log(Error));
+  });
+  it('Verifica se o parâmetro referente aos está em um valor entre 0 e 59', () => {
+    expect(getOpeningHours('Tuesday', '12:69-PM')).toThrow(console.log(Error));
+  });
+  it('Verifica se o parâmetro referente a AM/PM', () => {
+    expect(getOpeningHours('Wednesday', '13:71-ZM')).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+  });
 });
