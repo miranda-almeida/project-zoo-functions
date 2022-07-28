92288377be15761d0ff1d1c16fac39c5701a7036
miranda <magui@duck.com>
2022-07-28 15:39:58 -0300

    requisito 1: getSpeciesById

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui 
+function getSpeciesByIds(...ids) {
+  return data.species.filter((search) => ids.includes(search.id));
 }
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
