ad4351b8a96612d25edc745342defb58caa8fe39
miranda <magui@duck.com>
2022-07-29 14:25:49 -0300

    requisito 8: função calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,33 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const entries = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((person) => {
+    if (person.age < 18) {
+      entries.child += 1;
+    }
+    if (person.age >= 18 && person.age < 50) {
+      entries.adult += 1;
+    }
+    if (person.age >= 50) {
+      entries.senior += 1;
+    }
+  });
+  return entries;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.entries(entrants).length === 0) {
+    return 0;
+  }
+  let values = 0;
+  const children = Object.values(countEntrants(entrants))[0] * data.prices.child;
+  const adults = Object.values(countEntrants(entrants))[1] * data.prices.adult;
+  const seniors = Object.values(countEntrants(entrants))[2] * data.prices.senior;
+  values += children;
+  values += adults;
+  values += seniors;
+  return values;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,39 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+let officeHour;
+let exhibition;
 
+// const days = {
+//   Monday: { officeHour, exhibition },
+//   Tuesday: { officeHour, exhibition },
+//   Wednesday: { officeHour, exhibition },
+//   Thursday: { officeHour, exhibition },
+//   Friday: { officeHour, exhibition },
+//   Saturday: { officeHour, exhibition },
+//   Sunday: { officeHour, exhibition },
+// };
+const infos = {
+  officeHour,
+  exhibition,
+};
+
+const daySchedule = (day) => {
+  if (day === 'Monday') {
+    return { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+  }
+  const animalAvailable = data.species.filter(({ availability }) => availability.includes(day));
+  const matchOpenHour = data.hours[day].open;
+  const matchCloseHour = data.hours[day].close;
+  officeHour = `Open from ${matchOpenHour}am until ${matchCloseHour}pm`;
+  exhibition = animalAvailable.map(({ name }) => name);
+  return infos;
+};
+console.log(daySchedule('Tuesday')); // returning undefined
+
+function getSchedule(scheduleTarget) {
+  const finder = data.species.find((names) => names.name === scheduleTarget);
+  return finder.availability;
+
+}
+// console.log(getSchedule('lions'));
 module.exports = getSchedule;
