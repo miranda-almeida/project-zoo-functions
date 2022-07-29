cc750c10c3b7f3ecee2a27a7c6848888075d56be
miranda <magui@duck.com>
2022-07-28 21:16:09 -0300

    requisito 16: implementa 100% de testes na função getOpeningHours

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -19,7 +19,18 @@ describe('Testes da função getOpeningHours', () => {
   it('Verifica se o parâmetro referente a AM/PM', () => {
     expect(() => getOpeningHours('Wednesday', '13:71-ZM')).toThrow('The abbreviation must be \'AM\' or \'PM\'');
   });
-  it('Verifica se o parâmetro referente ao dia da semana', () => {
+  it('Verifica se o primeiro parâmetro recebe um dia da semana válido', () => {
     expect(() => getOpeningHours('dia', '13:11-AM')).toThrow(console.log(Error));
+  });
+  it('Verifica se o zoo retorna como aberto no domingo às 8:00AM', () => {
+    expect(getOpeningHours('')).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
   });
 });
